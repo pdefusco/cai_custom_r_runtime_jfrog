6910b433f3d17a90041b30b74352b30c69564dcf
pdefusco <pauldefusco@gmail.com>
2026-04-13 12:16:57 +0200

    test script}

---
+++ sparklyrtest.R
@@ -0,0 +1,73 @@
+#****************************************************************************
+# (C) Cloudera, Inc. 2020-2026
+#  All rights reserved.
+#
+#  Applicable Open Source License: GNU Affero General Public License v3.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# #  Author(s): Paul de Fusco
+#***************************************************************************/
+
+install.packages("dplyr")
+
+library(sparklyr)
+library(dplyr)
+
+# Spark configuration
+config <- spark_config()
+config$spark.kerberos.access.hadoopFileSystems <- "s3a://goes-se-sandbox/"
+config$spark.executor.cores = 1
+config$spark.executor.memory = "2g"
+
+# Connect to Spark
+sc <- spark_connect(config = config)
+
+# -----------------------------
+# Create a simple dataframe in R
+# -----------------------------
+local_df <- data.frame(
+  id = c(1, 2, 3),
+  name = c("Alice", "Bob", "Charlie"),
+  score = c(85, 90, 95)
+)
+
+# Copy it to Spark
+spark_df <- copy_to(sc, local_df, "example_table", overwrite = TRUE)
+
+# View the Spark dataframe
+spark_df %>% collect()
+
+# Optional: do a simple transformation
+spark_df %>%
+  filter(score > 85) %>%
+  arrange(desc(score)) %>%
+  collect()
